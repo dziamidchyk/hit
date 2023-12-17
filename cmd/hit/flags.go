77a0380b56b6546fc771741a81fc97d69b1dbd31
Arseni Demidchik <demidchik.arseniy@gmail.com>
2023-12-17 13:10:20 +0300

    refactor: parse flags using flag package

--- cmd/hit/flags.go
+++ cmd/hit/flags.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
+	"flag"
 )
 
 type flags struct {
@@ -13,42 +9,10 @@ type flags struct {
 	n, c int
 }
 
-type parseFunc func(string) error
-
-func (f *flags) parse() (err error) {
-	parsers := map[string]parseFunc{
-		"url": f.urlVar(&f.url),
-		"n":   f.intVar(&f.n),
-		"c":   f.intVar(&f.c),
-	}
-	for _, arg := range os.Args[1:] {
-		n, v, ok := strings.Cut(arg, "=")
-		if !ok {
-			continue
-		}
-		parse, ok := parsers[strings.TrimPrefix(n, "-")]
-		if !ok {
-			continue
-		}
-		if err := parse(v); err != nil {
-			err = fmt.Errorf("invalid value %q for flag %s: %w", v, n, err)
-			break
-		}
-	}
-	return err
-}
-
-func (f *flags) urlVar(p *string) parseFunc {
-	return func(s string) error {
-		_, err := url.Parse(s)
-		*p = s
-		return err
-	}
-}
-
-func (f *flags) intVar(p *int) parseFunc {
-	return func(s string) (err error) {
-		*p, err = strconv.Atoi(s)
-		return err
-	}
+func (f *flags) parse() error {
+	flag.StringVar(&f.url, "url", "", "HTTP server `URL` to make requests (required)")
+	flag.IntVar(&f.n, "n", f.n, "Number of requests to make")
+	flag.IntVar(&f.c, "c", f.c, "Concurrency level")
+	flag.Parse()
+	return nil
 }
--- cmd/hit/hit.go
+++ cmd/hit/hit.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"runtime"
 )
 
-const (
-	bannerText = `
+const bannerText = `
  __  __     __     ______
 /\ \_\ \   /\ \   /\__  _\
 \ \  __ \  \ \ \  \/_/\ \/
  \ \_\ \_\  \ \_\    \ \_\
   \/_/\/_/   \/_/     \/_/
 `
-	usageText = `
-Usage:
-  -url
-       HTTP server URL to make requests (required)
-  -n
-       Number of requests to make
-  -c
-       Concurrency level`
-)
 
 func banner() string { return bannerText[1:] }
-func usage() string  { return usageText[1:] }
 
 func main() {
 	f := &flags{
@@ -33,8 +22,7 @@ func main() {
 		c: runtime.NumCPU(),
 	}
 	if err := f.parse(); err != nil {
-		fmt.Println(usage())
-		log.Fatal(err)
+		os.Exit(1)
 	}
 	fmt.Println(banner())
 	fmt.Printf("Making %d requests to %s with a concurrency level of %d.\n", f.n, f.url, f.c)
